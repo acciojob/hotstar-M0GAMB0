94395b21a57f66cf063b86d0acbfb5e68a326e3d
Saikumar Hangal <saikumarhangal99@gmail.com>
2023-05-05 21:31:02 +0530

    all services corrected

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -14,8 +14,14 @@ public class ProductionHouseService {
     ProductionHouseRepository productionHouseRepository;
 
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
-        ProductionHouse productionHouse=new ProductionHouse(productionHouseEntryDto.getName());
-        return  productionHouse.getId();
+        ProductionHouse productionHouse = new ProductionHouse(productionHouseEntryDto.getName());
+
+        productionHouse.setRatings(0.0);
+
+        ProductionHouse addedProductionHouse = productionHouseRepository.save(productionHouse);
+
+
+        return addedProductionHouse.getId();
     }
 
 
